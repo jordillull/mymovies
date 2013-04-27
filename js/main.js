758183f765008cbf4ad65f7c9173f2d4b28a9f47
jordillull <worm13@gmail.com>
2013-04-27 19:02:28 +0200

    added fontawesome

--- js/main.js
+++ js/main.js
@@ -1,1 +1,400 @@
-
+var cinesa = [
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 2", 
+            "times": [
+              "20:00", 
+              "22:00", 
+              "00:01"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "AYER NO TERMINA NUNCA"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 19", 
+            "times": [
+              "20:30", 
+              "22:40", 
+              "00:50"
+            ]
+          }, 
+          {
+            "room": "Sala 3", 
+            "times": [
+              "19:30", 
+              "22:00", 
+              "00:10"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "COMBUSTI\u00d3N"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 6", 
+            "times": [
+              "20:25"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "DANDO LA NOTA"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 14", 
+            "times": [
+              "22:40", 
+              "00:50"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "EFECTOS SECUNDARIOS"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 15", 
+            "times": [
+              "20:30", 
+              "22:45", 
+              "01:00"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "G.I JOE 2: LA VENGANZA"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "3D RealD", 
+        "times": [
+          {
+            "room": "Sala 12", 
+            "times": [
+              "20:45"
+            ]
+          }, 
+          {
+            "room": "Sala 18", 
+            "times": [
+              "18:45", 
+              "00:15"
+            ]
+          }, 
+          {
+            "room": "Sala 9", 
+            "times": [
+              "22:00"
+            ]
+          }
+        ]
+      }, 
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 10", 
+            "times": [
+              "19:30", 
+              "22:20"
+            ]
+          }, 
+          {
+            "room": "Sala 12", 
+            "times": [
+              "23:30"
+            ]
+          }, 
+          {
+            "room": "Sala 18", 
+            "times": [
+              "21:30"
+            ]
+          }, 
+          {
+            "room": "Sala 9", 
+            "times": [
+              "19:10", 
+              "00:50"
+            ]
+          }
+        ]
+      }, 
+      {
+        "type": "IMAX \u00ae 3D", 
+        "times": [
+          {
+            "room": "Sala 11", 
+            "times": [
+              "20:00", 
+              "22:45"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "IRON MAN 3"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "3D RealD", 
+        "times": [
+          {
+            "room": "Sala 8", 
+            "times": [
+              "19:20"
+            ]
+          }
+        ]
+      }, 
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 4", 
+            "times": [
+              "20:20"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "LOS CROODS"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 17", 
+            "times": [
+              "20:15", 
+              "22:30", 
+              "00:35"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "MEMORIAS DE UN ZOMBIE ADOLESCENTE"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 1", 
+            "times": [
+              "19:00", 
+              "22:00", 
+              "00:35"
+            ]
+          }, 
+          {
+            "room": "Sala 20", 
+            "times": [
+              "20:45", 
+              "23:30"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "OBLIVION"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 7", 
+            "times": [
+              "19:00"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "OZ, UN MUNDO DE FANTAS\u00cdA"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 16", 
+            "times": [
+              "18:30", 
+              "20:30", 
+              "22:30", 
+              "00:40"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "POSESI\u00d3N INFERNAL"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 10", 
+            "times": [
+              "00:55"
+            ]
+          }, 
+          {
+            "room": "Sala 14", 
+            "times": [
+              "20:30"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "TESIS SOBRE UN HOMICIDIO"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 7", 
+            "times": [
+              "22:00", 
+              "00:35"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "THE HOST"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 6", 
+            "times": [
+              "22:40", 
+              "00:50"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "TIERRA PROMETIDA"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 4", 
+            "times": [
+              "22:30", 
+              "00:40"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "TIPOS LEGALES"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 8", 
+            "times": [
+              "21:30", 
+              "00:01"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "UN AMOR ENTRE DOS MUNDOS"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 13", 
+            "times": [
+              "20:15", 
+              "22:15", 
+              "00:20"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "UN GRAN EQUIPO"
+  }, 
+  {
+    "sessions": [
+      {
+        "type": "Digital", 
+        "times": [
+          {
+            "room": "Sala 5", 
+            "times": [
+              "20:30", 
+              "22:45", 
+              "01:00"
+            ]
+          }
+        ]
+      }
+    ], 
+    "title": "UN LUGAR DONDE REFUGIARSE"
+  }
+]
--- scrapping/cinesa.py
+++ scrapping/cinesa.py
@@ -4,16 +4,61 @@ import urllib2
 import json
 from datetime import date
 from bs4 import BeautifulSoup 
-URL="http://www.cinesa.es/Cines/horariosPorDia/234/0"
+ARTEA = 260
+AS_CANCELAS = 1200 
+AUGUSTA = 200
+BAHIA_SANTANDER = 270
+BARNASUD = 1037
+BONAIRE = 721
+CAPITOL = 1027
+DIAGONAL = 120
+DIAGONAL_MAR = 232
+EL_FORO =  250
+EL_MUELLE = 761
+EQUINOCCIO = 631
+FESTIVAL_PARK = 234
+GRANCASA = 611
+HERON_CITY_BARCELONA = 291
+LA_CANADA = 280
+LA_FARGA = 113
+LA_GAVIA = 1039
+LA_MAQUINISTA = 661
+LA_MORALEJA = 601
+LAS_ROSAS = 196
+LAS_ROZAS_HERON_CITY = 236
+LORANCA = 192
+LOS_BARRIOS = 1054
+MANOTERAS = 1052
+MAREMAGNUM = 112
+MARINEDA_CITY = 1047
+MATARO_PARC = 275
+MAX_OCIO = 1056
+MENDEZ_ALVARO = 1050 
+NASSICA = 1051
+NUEVA_CONDOMINA = 1033
+PARC_VALLES = 231
+PARQUE_PRINCIPADO = 701
+PARQUESUR = 190
+PLAZA_DE_ARMAS = 651
+PRINCIPE_PIO = 781
+PROYECCIONES = 331
+PUERTO_VENECIA = 1100
+SANT_CUGAT = 110
+SIETE_PALMAS = 741
+XANADU = 311
+ZARATAN = 1053
+ZUBIARTE = 1055
+
+URL="http://www.cinesa.es/Cines/horariosPorDia/"
 
 
 
-def listing(day=False, verbose=False):
+def listing(area=FESTIVAL_PARK, day=False, verbose=False):
   def v(str):
     if verbose:
       print str       
       
-  soup = BeautifulSoup(urllib2.urlopen(URL)) 
+  soup = BeautifulSoup(urllib2.urlopen(URL+str(area))) 
   
   if not day:
     day = date.today().strftime("%d/%m")
@@ -37,15 +82,17 @@ def listing(day=False, verbose=False):
       v("##"+type_name)
       for room in type.find("ul", "n_sala").find_all("li", recursive=False): 
         room_name = room.find("div","wrapper_sala").getText()
+        times = []
         v("----"+room_name)
         for hour in room.find("ul", "horas").find_all("li", recursive=False):
           v("......"+hour.getText())
-          session['times'].append({'room' : room_name, 'time' : hour.getText()})
+          times.append(hour.getText())
+        session['times'].append({'room' : room_name, 'times' : times})
       result['sessions'].append(session)
     results.append(result)
   
   return results
 
 if __name__ == "__main__":
-  l = listing()
+  l = listing(FESTIVAL_PARK)
   print json.dumps(l, indent=2)
